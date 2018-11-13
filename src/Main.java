51185985f483cfff9358a78fa8e8daae7006802a
nnzhuilian <609117168@qq.com>
2018-11-13 21:41:55 +0800

    11-13 night

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,20 @@
 import java.io.File;
 
 public class Main {
-	public static final int CYCN=5;//周期数
-	public static final int WINDOW=8;//窗口大小
+	public static final int CYCN=6;//周期数
+	public static final int WINDOW=7;//窗口大小
 	public static void main(String args[]) throws Exception {
 		Masking[] com=new Masking[26];
 		int cc=0;
-		File dirfile = new File("C:\\HXH软件\\JavaP\\Mytestsh\\实验数据\\data");
+		File dirfile = new File(".\\实验数据\\data");
 		File files[] = dirfile.listFiles();
 		for (File file : files) {
+			System.out.println("////////////////-"+cc+"-begin-//////////////////////");
 			DataInitial ds1 = new DataInitial(
 					new Readfile(file).readfile().getdata());
 			Data[] resultdatas1 = ds1.getDatas();//初始化数据
 			int gl=new GetLength(resultdatas1,CYCN).getBitLength();//获取周期对应比特数
+			System.out.println("bit长度："+gl);
 			
 			FeatureExtractor featureExtractor1 = new FeatureExtractor(WINDOW, resultdatas1,gl);
 			
@@ -22,16 +24,16 @@ public class Main {
 			 * for(int i=0;i<resultdatas1.length;i++){
 			 * System.out.print(resultdatas1[i].getCompare()); }
 			 */
-			System.out.println(" ");
 			Timeround tr=new Timeround(WINDOW, resultdatas1,CYCN,gl);
 			MaskSeed ms=new MaskSeed(tr.getCycle(),gl);
 			String mask=ms.printmask(WINDOW);
 			/////////////////////////////////////////////////////////////////////////////
 		    Masking mk=new Masking(feature, mask);
 		    //System.out.println(mk.Masker());
+		    System.out.println("////////////////-"+cc+"-end-//////////////////////");
+		    System.out.println(" ");
+		    
 		    com[cc++]=mk;   
-		    
-		    
 		}	
 		for(int k=0;k<13;k++){
 			System.out.println(compare(com[2*k].MaskerI(),com[2*k+1].MaskerI()));
--- src/Timeround.java
+++ src/Timeround.java
@@ -55,6 +55,10 @@ public class Timeround {
 		return (-y1*(x2-x1))/(y2-y1)+x1;
 	}
 	public double getCycle(){
+		System.out.println("周期计算开始位置："+getFirstDown());
+		System.out.println("周期计算结束位置："+getLastDown());
+		System.out.println("总周期时间："+(getLastDown()-getFirstDown()));
+		System.out.println("周其长度："+((getLastDown()-getFirstDown())/cycnumbers));
 		return (getLastDown()-getFirstDown())/cycnumbers;
 	}
 	
